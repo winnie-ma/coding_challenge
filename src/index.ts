76d51e3c12de464d23e3a25c1c3df70151ebbbe2
winnie-ma <xuma8821@gmail.com>
2024-10-10 13:08:05 +1100

    feat: realize command line arguments extraction

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-// The CLI entry point should live here!
+import { reverseSentence } from "./reverse";
+
+// Extract command-line arguments
+const args = process.argv.slice(2);
+console.log(args, process.argv);
+
+if (args.length === 0 || args.length > 3) {
+    console.error("Pls provide valid args in command line");
+    process.exit(1);
+}
+
+// Find the index of the first non-flag argument (the sentence)
+const sentenceIndex = args.findIndex(arg => !arg.startsWith("--"));
+const sentence = args[sentenceIndex];
+console.log(sentenceIndex, sentence)
+
+//Flags for whether reverse word/letters
+const isReverseWords = args.includes("--word");
+const isReverseLetters = args.includes("--letter");
+
+// Check if sentence was provided
+if (!sentence) {
+    console.error("Pls provide a sentence to reverse.");
+    process.exit(1);
+}
+
+
+// Call reverseSentence and print the result
+// const result = reverseSentence(sentence, isReverseWords, isReverseLetters);
+// console.log(result);
